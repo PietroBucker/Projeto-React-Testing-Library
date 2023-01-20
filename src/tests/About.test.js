2de7f1031eea323f9c2bc7d3100ead378cdf162d
Pietro Bucker <pietro_177@hotmail.com>
2023-01-20 19:25:00 -0300

    requisito 3 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -20,11 +20,6 @@ describe('Teste o componente <About.js />.', () => {
     expect(title).toBeInTheDocument();
   });
 
-  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex;', () => {
-    const title = screen.getAllByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon' && 'One can filter Pokémon by type, and see more details for each one of them');
-    console.log(title.length);
-  });
-
   test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
     const img = screen.getByAltText(/pokédex/i);
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+
+beforeEach(() => renderWithRouter(<FavoritePokemon />));
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  test('Ao favoritar a partir da página de detalhes: Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos;', () => {
+    const textNotFavorite = screen.getByText('No favorite Pokémon found');
+    expect(textNotFavorite).toBeInTheDocument();
+  });
+
+  test('Teste se apenas são exibidos os Pokémon favoritados.', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [pokemonList[0]] } />);
+    const favorito = screen.getByText('Pikachu');
+    expect(favorito).toBeInTheDocument();
+  });
+});
