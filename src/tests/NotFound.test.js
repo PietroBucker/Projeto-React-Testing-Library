e5041629caac1cf093362fbfe1d2c45812086623
Pietro Bucker <pietro_177@hotmail.com>
2023-01-20 19:33:02 -0300

    requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+beforeEach(() => renderWithRouter(<NotFound />));
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    const title = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const img = screen.getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
